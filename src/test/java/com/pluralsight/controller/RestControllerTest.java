f88c6fc2ea8802e3854110b4ec4bbc48f917fd1f
dmalkinski <dmalkinski@gmail.com>
2017-12-04 12:06:23 -0500

    Pluralsight spring jdbc my changes

--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -14,11 +14,23 @@ import org.junit.Test;
 public class RestControllerTest {
 
 	@Test(timeout=3000)
+	public void testCreateRide() {
+		RestTemplate restTemplate = new RestTemplate();
+		Ride ride = new Ride();
+		ride.setName("Yellow Fork Trail");
+		ride.setDuration(38);
+		
+		ride = restTemplate.postForObject("http://localhost:8085/ride_tracker/ride", ride, Ride.class);
+		
+		System.out.println("Ride: " + ride);
+	}
+	
+	@Test(timeout=3000)
 	public void testGetRides() {
 		RestTemplate restTemplate = new RestTemplate();
 
 		ResponseEntity<List<Ride>> ridesResponse = restTemplate.exchange(
-				"http://localhost:8080/ride_tracker/rides", HttpMethod.GET,
+				"http://localhost:8085/ride_tracker/rides", HttpMethod.GET,
 				null, new ParameterizedTypeReference<List<Ride>>() {
 				});
 		List<Ride> rides = ridesResponse.getBody();
@@ -27,4 +39,53 @@ public class RestControllerTest {
 			System.out.println("Ride name: " + ride.getName());
 		}
 	}
+	
+	
+	@Test(timeout=3000)
+	public void testGetRide() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Ride ride = restTemplate.getForObject("http://localhost:8085/ride_tracker/ride/1",  Ride.class);
+		
+		System.out.println("Ride name: " + ride.getName());
+	}
+	
+	@Test(timeout=3000)
+	public void testUpdateRide() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Ride ride = restTemplate.getForObject("http://localhost:8085/ride_tracker/ride/1",  Ride.class);
+		
+		ride.setDuration(ride.getDuration() + 1);
+		
+		restTemplate.put("http://localhost:8085/ride_tracker/ride", ride);
+		
+		
+		System.out.println("Ride name: " + ride.getName());
+	}
+	
+	@Test(timeout=3000)
+	public void testBatchUpdate() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		restTemplate.getForObject("http://localhost:8085/ride_tracker/batch",  Object.class);
+		
+	}
+	
+	@Test(timeout=3000)
+	public void testDelete() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		restTemplate.delete("http://localhost:8085/ride_tracker/delete/5",  Object.class);
+		
+	}
+	
+	@Test(timeout=3000)
+	public void testException() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		restTemplate.getForObject("http://localhost:8085/ride_tracker/test",  Ride.class);
+		
+	}
+	
 }
